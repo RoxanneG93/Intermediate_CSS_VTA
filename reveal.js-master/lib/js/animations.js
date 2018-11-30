d6d7930b0c6452ed29bde2bce3f66198ef3ed0ec
Roxanne Garcia <rxrgeneration@gmail.com>
2018-11-29 21:03:30 -0600

    added new theme and text animation

--- reveal.js-master/lib/js/animations.js
+++ reveal.js-master/lib/js/animations.js
@@ -1,6 +1,5 @@
-anime({
-    targets: 'p',
-    translateX: 250, // Animate all divs translateX property to 250px
-    scale: 2, // Animate all divs scale to 2
-    rotate: '1turn' // Animate all divs rotation to 1 turn
-});
+// Reveal.addEventListener( 'slidechanged', function( event ) {
+//     const logo = document.getElementById('googleAnimate');
+
+//     logo.styl
+// } );
